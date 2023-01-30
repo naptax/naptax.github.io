bac53a935b4b428bd64675b5e92310bec18d9759
NaPtaX <naptax@proton.me>
2023-01-30 13:10:58 +0100

    add content for GULOADER post

--- code/BN_xor/xor.py
+++ code/BN_xor/xor.py
@@ -2,25 +2,20 @@ from binaryninja import *
 
 xor_key = struct.pack('<I',0x919E1E2E)
 code_offset = 0x0000014E
-code_before = bytearray
-code_after  = []
+code_before = []
+out  = []
 
 view = bv
 
 start_addr = view.start + code_offset
 end_addr = view.end
 
-code_before = view.read(start_addr, end_addr - start_addr)
-print(f'Binaire (size = {len(code_before)} ) AVANT le xor:\t', code_before[0:24])
+code_before = view.read(start_addr, end_addr - start_addr).hex()
+print(code_before)
 
 
-for i in range(len(code_before)):
-    code_after.append(code_before[i] ^ xor_key[i % len(xor_key)])
 
-print(f'Binaire (size = {len(code_after)} ) APRES le xor:\t', bytearray(code_after[0:24]))
-
-
-view.write(start_addr, bytearray(code_after))
+# view.write(start_addr, bytearray(code_after))
 
 view.update_analysis_and_wait()
-view.save('c:\\temp\part2.bin')
+# view.save('c:\\temp\part2.bin')
